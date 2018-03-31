84690c817484c9cac0745c18f4782f5fab3a14e1
Andrew Redd <aredd@andrew.cmu.edu>
2018-03-31 16:09:10 -0400

    builds basic explorartory analysis

--- R Files/exploration.R
+++ R Files/exploration.R
@@ -1,7 +1,28 @@
 library(readr)
 library(dplyr)
 library(stringr)
+library(mice)
 
 setwd("C:/Users/andre/GoogleDrive/MLPSProject")
 
+# load data
 data = read_tsv("Data/train.tsv")
+
+# replace no item descriptions with nas
+data$item_description[data$item_description == "No description yet"] = NA
+
+# which columns have nas
+apply(data, 2, function(x) any(is.na(x)))
+
+# total number of nas
+data %>% is.na() %>% colSums()
+
+# View item descriptions by category
+data %>% filter(is.na(item_description)) %>%  group_by(category_name) %>% count() 
+
+# View category structure
+data %>% 
+  select(category_name) %>% 
+  unique() %>% 
+  mutate(num = str_count(category_name, pattern = "/")) %>% 
+  filter(num != 2) %>% View()
