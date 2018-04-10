b5c66101a8553f72a91ead376c2863e66d227f83
Andrew Redd <aredd@andrew.cmu.edu>
2018-04-10 14:59:38 -0400

    adds pruning to the processing step as well as a tfidf transformation

--- R Files/data processing.R
+++ R Files/data processing.R
@@ -10,6 +10,8 @@ setwd("C:/Users/andre/OneDrive/School/CMU/Spring 2018/Machine Learning for Probl
 # load data
 data = read_tsv("Data/train.tsv")
 
+data = data %>% filter(!is.na(price))
+
 # replace no item descriptions with nas
 data$item_description[data$item_description == "No description yet"] = NA
 data$item_description[is.na(data$item_description)] = "NA"
@@ -25,15 +27,9 @@ data = data %>%
 
 write_tsv(data %>% select(-category_list), "Data/train_clean.tsv")
 
-set.seed(95828)
-# dev_size = 1000000
-# dev_ids = sample.int(nrow(data), dev_size)
-# dev_data = data[dev_ids,]
+data = read_tsv("Data/train_clean.tsv")
 
-# CODE DOESN'T RUN ON FULL DATA SET IN UNDER 15 MINUTES
-# create a length column for description and name NOTE: Counts NAs as a length 1 so if this ends up being important recode
-# dev_data = dev_data %>% mutate(description_len = map_int(item_description, ~ .x %>% strsplit(split = " ") %>% unlist() %>% length()),
-#               name_len = map_int(name, ~ .x %>% strsplit(split = " ") %>% unlist() %>% length()))
+set.seed(95828)
 
 it_train = itoken(data$item_description,
                   preprocessor=tolower,
@@ -41,18 +37,28 @@ it_train = itoken(data$item_description,
                   ids = data$train_id,
                   progressbar = FALSE)
 
-stop_words = c("a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves")
+stop_words = c("na", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves")
 
 vocab = create_vocabulary(it_train, stopwords = stop_words)
 
-vectorizer = vocab_vectorizer(vocab)
+pruned_vocab = prune_vocabulary(vocabulary = vocab, 
+                                term_count_min = 10, 
+                                doc_proportion_max = 0.5, 
+                                doc_proportion_min = 0.001)
 
-dtm_final = create_dtm(it_train, vectorizer)
+vectorizer = vocab_vectorizer(pruned_vocab)
+
+dtm_final_non_tfidf = create_dtm(it_train, vectorizer)
+
+tfidf = TfIdf$new()
+dtm_final = fit_transform(dtm_final_non_tfidf, tfidf)
 
 colnames_dtm_final = rownames(t(dtm_final))
 
-write(colnames_dtm_final, file = "Data/colnames_train_term_matrix.txt")
-writeMM(dtm_final, file = "Data/train_term_matrix.mtx")
+setwd("C:/Users/andre/GoogleDrive/MLPSProject/")
+
+write(colnames_dtm_final, file = "Data/colnames_train_term_matrix_pruned.txt")
+writeMM(dtm_final, file = "Data/train_term_matrix_pruned.mtx")
 
 
 
