e3937330b21bd4d6385dc1a27249f46c342f37af
Andrew Redd <aredd@andrew.cmu.edu>
2018-05-01 19:59:59 -0400

    moves data processing to main file

--- data processing.R
+++ data processing.R
@@ -31,15 +31,21 @@ data = read_tsv("Data/train_clean.tsv")
 
 set.seed(95828)
 
+# insert porter stemmer function
+
 it_train = itoken(data$item_description,
                   preprocessor=tolower,
                   tokenizer=word_tokenizer,
                   ids = data$train_id,
                   progressbar = FALSE)
 
+# could be replaced with a more comprehensive list in tidy text
+# could be replaced with a functional stop word list
 stop_words = c("na", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves")
 
 vocab = create_vocabulary(it_train, stopwords = stop_words)
+
+# potentially remove and let tfidf 
 
 pruned_vocab = prune_vocabulary(vocabulary = vocab, 
                                 term_count_min = 10, 
@@ -48,7 +54,11 @@ pruned_vocab = prune_vocabulary(vocabulary = vocab,
 
 vectorizer = vocab_vectorizer(pruned_vocab)
 
+# potentially dump as a dataset to compare tfidf transformation vs count frequency
+
 dtm_final_non_tfidf = create_dtm(it_train, vectorizer)
+
+# drop low values from tfidf
 
 tfidf = TfIdf$new()
 dtm_final = fit_transform(dtm_final_non_tfidf, tfidf)
